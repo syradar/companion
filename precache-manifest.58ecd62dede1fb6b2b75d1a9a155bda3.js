4fb7a91b61dd01be193366eb4a0080ecd2753326
syradar <syradar@users.noreply.github.com>
2020-05-12 21:41:35 +0000

    deploy: 395afc50fef9214afcbf9aa4a57e59bccf3a1291

---
+++ precache-manifest.58ecd62dede1fb6b2b75d1a9a155bda3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "04825ac0d2a4a3508055ef7f188646b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "623cf87048ff60b45e28",
+    "url": "/static/css/main.47d6708c.chunk.css"
+  },
+  {
+    "revision": "623cf87048ff60b45e28",
+    "url": "/static/js/main.623cf870.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "7aa09ec89c957a98c55b",
+    "url": "/static/js/vendors~main.7aa09ec8.chunk.js"
+  }
+]);
